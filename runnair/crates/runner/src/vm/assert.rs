c0afdf77be24e8e3bfd3f680df4e7ac128c5e479
Elin Tulchinsky <elin@starkware.co>
2024-12-01 19:17:12 +0200

    feat: implement `assert_[ap/fp]_add_imm_[ap/fp][_appp]` instructions

--- runnair/crates/runner/src/vm/assert.rs
+++ runnair/crates/runner/src/vm/assert.rs
@@ -51,36 +51,68 @@ fn assign_or_assert_add_on_memory(
     };
 }
 
-// TODO: handle imm.
+fn assign_or_assert_add_on_memory_with_imm(
+    memory: &mut Memory,
+    dest: MaybeRelocatableAddr,
+    imm: MaybeRelocatableAddr,
+    op1: MaybeRelocatableAddr,
+) {
+    match (memory.get(dest), memory.get(op1)) {
+        (Some(dest_val), Some(op0_val)) => {
+            assert_eq!(dest_val, op0_val + imm, "Assertion failed.");
+        }
+        (None, Some(op0_val)) => {
+            let deduced_value = op0_val + imm;
+            memory.insert(dest, deduced_value);
+        }
+        (Some(dest_val), None) => {
+            let deduced_value = dest_val - imm;
+            memory.insert(op1, deduced_value);
+        }
+        _ => panic!("Cannot deduce more than one operand"),
+    };
+}
+
+fn assert_or_insert(
+    memory: &mut Memory,
+    state: State,
+    dest: &str,
+    op1: &str,
+    op2: &str,
+    args: InstructionArgs,
+) {
+    if op1 == "imm" {
+        let addresses = resolve_addresses(state, &[dest, op2], &[args[0], args[2]]);
+        assign_or_assert_add_on_memory_with_imm(memory, addresses[0], args[1].into(), addresses[1]);
+    } else {
+        let addresses = resolve_addresses(state, &[dest, op1, op2], &args);
+        assign_or_assert_add_on_memory(memory, addresses[0], addresses[1], addresses[2]);
+    }
+}
+
 // TODO: handle mul.
 macro_rules! define_assert {
     ($dest:ident, $op1:ident, $op2:ident) => {
         paste! {
-            /// Function without incrementing `ap`: `assert_[ap/fp]_add_[ap/fp]_[ap/fp]`.
+            /// Function without incrementing `ap`: `assert_[ap/fp]_add_[ap/fp/imm]_[ap/fp]`.
             pub fn [<assert_ $dest _add_ $op1 _ $op2>] (
                 memory: &mut Memory,
                 state: State,
                 args: InstructionArgs,
             ) -> State {
                 let (dest, op1, op2) = (stringify!($dest), stringify!($op1), stringify!($op2));
-
-                let addresses = resolve_addresses(state, &[dest, op1, op2], &args);
-                assign_or_assert_add_on_memory(memory, addresses[0], addresses[1], addresses[2]);
-
+                assert_or_insert(memory, state, dest, op1, op2, args);
                 state.advance()
             }
 
-            /// Function with incrementing `ap`: `assert_[ap/fp]_add_[ap/fp]_[ap/fp][_appp]`.
+            /// Function with incrementing `ap`: `assert_[ap/fp]_add_[ap/fp/imm]_[ap/fp][_appp]`.
             pub fn [<assert_ $dest _add_ $op1 _ $op2 _appp>] (
                 memory: &mut Memory,
                 state: State,
                 args: InstructionArgs,
             ) -> State {
                 let (dest, op1, op2) = (stringify!($dest), stringify!($op1), stringify!($op2));
-
-                let addresses = resolve_addresses(state, &[dest, op1, op2], &args);
-                assign_or_assert_add_on_memory(memory, addresses[0], addresses[1], addresses[2]);
-
+                assert_or_insert(memory, state, dest, op1, op2, args);
                 state.advance_and_increment_ap()
             }
         }
@@ -91,9 +123,13 @@ define_assert!(ap, ap, ap);
 define_assert!(ap, ap, fp);
 define_assert!(ap, fp, ap);
 define_assert!(ap, fp, fp);
+define_assert!(ap, imm, ap);
+define_assert!(ap, imm, fp);
 define_assert!(fp, ap, ap);
 define_assert!(fp, ap, fp);
 define_assert!(fp, fp, ap);
 define_assert!(fp, fp, fp);
+define_assert!(fp, imm, ap);
+define_assert!(fp, imm, fp);
 
 // TODO: add tests.
